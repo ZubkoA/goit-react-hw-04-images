4365e4631406cc7873d6ae135376fabb14e19ad1
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-05-22 22:24:43 +0300

    mistake

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,14 +31,13 @@ const App = () => {
         return Promise.reject(new Error('Did not find'));
       })
       .then(data => {
-        // if (hits === null) return setHits(data);
-        // return;
-        // setHits(prev => [...prev, data]);
-        hits === null ? setHits(data) : setHits(prev => [...prev, data]);
+        hits === null
+          ? setHits(data.hits)
+          : setHits(prev => [...prev, ...data.hits]);
       })
       .catch(error => setError(error))
       .finally(() => setIsLoading(false));
-  }, [searchImg, page, hits]);
+  }, [page, isLoading, searchImg]);
 
   const closeModal = () => {
     setIsShowModal(false);
@@ -65,7 +64,7 @@ const App = () => {
       <Searchbar handleSearch={handleSearch} />
       {isLoading && <Loader />}
 
-      {hits !== null && <ImageGallery hits={hits} onData={savedModal} />}
+      {/* {hits !== null && <ImageGallery hits={hits} onData={savedModal} />} */}
       {hits?.length > 0 && <Button handleClick={handleAdd} title="Load more" />}
       {hits?.length === 0 && (
         <div style={{ fontSize: '24px', fontWeight: '600' }}>
@@ -78,83 +77,3 @@ const App = () => {
 };
 
 export default App;
-
-// class App extends Component {
-//   state = {
-//     searchImg: '',
-//     hits: null,
-//     isLoading: false,
-//     isShowModal: false,
-//     page: 1,
-//     perPage: 12,
-//     modal: { url: '', tags: '' },
-//     error: null,
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const prevName = prevState.searchImg;
-//     const prevPage = prevState.page;
-//     const { page, perPage, hits, searchImg } = this.state;
-
-//     if (prevName !== searchImg || prevPage !== page) {
-//       this.setState({ isLoading: true });
-//       getSearchImg(searchImg, page, perPage)
-//         .then(res => {
-//           if (res.ok) {
-//             return res.json();
-//           }
-//           return Promise.reject(new Error('Did not find'));
-//         })
-//         .then(data => {
-//           if (hits === null) return this.setState({ hits: data.hits });
-//           return this.setState({ hits: [...prevState.hits, ...data.hits] });
-//         })
-//         .catch(error => this.setState({ error }))
-//         .finally(() => this.setState({ isLoading: false }));
-//     }
-//   }
-//   closeModal = () => {
-//     this.setState({ isShowModal: false });
-//   };
-
-//   handleSearch = searchImg => {
-//     this.setState({ searchImg, hits: null, page: 1 });
-//   };
-
-//   handleAdd = () => {
-//     this.setState(prevState => {
-//       return {
-//         page: prevState.page + 1,
-//       };
-//     });
-//   };
-//   savedModal = (tags, url) => {
-//     this.setState({ modal: { tags, url }, isShowModal: true });
-//   };
-
-//   render() {
-//     const { hits, isLoading, isShowModal, modal, error, searchImg } =
-//       this.state;
-//     return (
-//       <div className={css.App}>
-//         <ToastContainer />
-//         {error && <h1>{error.message}</h1>}
-//         <Searchbar handleSearch={this.handleSearch} />
-//         {isLoading && <Loader />}
-
-//         {hits !== null && <ImageGallery hits={hits} onData={this.savedModal} />}
-//         {hits?.length > 0 && (
-//           <Button handleClick={this.handleAdd} title="Load more" />
-//         )}
-//         {hits?.length === 0 && (
-//           <div style={{ fontSize: '24px', fontWeight: '600' }}>
-//             No results {searchImg}!
-//           </div>
-//         )}
-//         {isShowModal && <Modal onClose={this.closeModal} modal={modal} />}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
