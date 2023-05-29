224b2cfa0f8d68f048d0f27308d6720c2650a816
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-05-29 11:11:05 +0300

    change loading

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,9 +2,15 @@ import { Bars } from 'react-loader-spinner';
 import React from 'react';
 
 const Loader = () => {
+  const style = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  };
   return (
-    <div>
-      <Bars />
+    <div style={style}>
+      <Bars color="#3f51b5" />
     </div>
   );
 };
